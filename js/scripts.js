cef885a4b310518c7d7c5e1ceac0c5ed395f5594
Jason Awbrey and Neil Larion <neil.larion@gmail.com>
2016-01-21 10:53:27 -0800

    add all passing tests

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,30 @@
-// Uncomment this to see the example spec test pass. Delete if you don't need it!
-// var helloWorld = function(){
-//   return false;
-// };
+var vowels = ["a", "e", "i", "o", "u"];
+
+
+var isVowel = function(char){
+    if(vowels.indexOf(char) !== -1){
+      return true;
+    }
+    return false;
+}
+
+var replaceVowel = function(char){
+  if (isVowel(char)){
+    return "-";
+  }else {
+    return char;
+  }
+}
+
+var puzzleParser = function(string){
+  string = string.split("");
+  for (var i = 0; i < string.length; i++) {
+    string[i] = replaceVowel(string[i]);
+  }
+  return string.join("");
+};
+
+
+
+ // var regEx = /[aeiouAEIOU]/g;
+ // string =  string.replace(regEx,"-");
--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,29 @@
-// Example spec. Uncomment if you want to see it run in spec-runner.html! Then uncomment the function in scripts.js
-// describe('helloWorld', function(){
-//   it("is going to be false", function(){
-//     expect(helloWorld()).to.equal(false);
-//   });
-// });
+describe("isVowel", function(){
+
+  it("Will check it see if the char is a vowel", function(){
+    expect(isVowel("")).to.equal(false);
+  });
+
+  it("Will return true if the char is a vowel", function(){
+    expect(isVowel("i")).to.equal(true);
+  });
+});
+
+
+describe("replaceVowel", function(){
+  it("Will take a vowel and replace with hyphen", function(){
+    expect(replaceVowel("a")).to.equal("-");
+  });
+
+  it("Will take a vowel and replace with hyphen", function(){
+    expect(replaceVowel("b")).to.equal("b");
+  });
+
+});
+
+
+describe("puzzleParser", function(){
+  it("Will take a string and parse string to remove vowels and replace with hyphen", function(){
+    expect(puzzleParser("some words")).to.equal("s-m- w-rds");
+  });
+});
